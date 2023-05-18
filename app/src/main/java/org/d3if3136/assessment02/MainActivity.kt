b92396f30c8a2d56e6f02e1ca3a9db29da676fe0
Annisa Nailiya Zahroh <annisanailiya@gmail.com>
2023-05-18 22:01:29 +0700

    halaman login

---
+++ app/src/main/java/org/d3if3136/assessment02/MainActivity.kt
@@ -0,0 +1,36 @@
+package org.d3if3136.assessment02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.TextUtils
+import android.view.LayoutInflater
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import org.d3if3136.assessment02.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.lanjutButton.setOnClickListener { welcome() }
+    }
+
+    private fun welcome() {
+        val nama = binding.namaEditText.text.toString()
+        if (TextUtils.isEmpty(nama)) {
+            Toast.makeText(this, R.string.nama_invalid, Toast.LENGTH_LONG).show()
+            return
+        }
+        val lanjut = nama
+
+        binding.namaTextView.text = getString(R.string.nama_x, lanjut)
+    }
+}
