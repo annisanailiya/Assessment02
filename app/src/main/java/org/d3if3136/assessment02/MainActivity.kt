8a0950b90fa5cd7755cda61038bc262c7dc458e8
Annisa Nailiya Zahroh <annisanailiya@gmail.com>
2023-05-19 11:56:53 +0700

    initial commit

--- app/src/main/java/org/d3if3136/assessment02/MainActivity.kt
+++ app/src/main/java/org/d3if3136/assessment02/MainActivity.kt
@@ -3,16 +3,18 @@ package org.d3if3136.assessment02
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
-import android.view.LayoutInflater
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
 import org.d3if3136.assessment02.databinding.ActivityMainBinding
+import org.d3if3136.assessment02.model.HasilInput
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private val viewModel: MainViewModel by lazy {
+        ViewModelProvider(this)[MainViewModel::class.java]
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,7 +32,12 @@ class MainActivity : AppCompatActivity() {
             return
         }
         val lanjut = nama
-
         binding.namaTextView.text = getString(R.string.nama_x, lanjut)
+    }
+
+    private fun showResult(result: HasilInput) {
+        if (result == null) return
+
+        binding.namaTextView.text = getString(R.string.nama_x,result.lanjut)
     }
 }
---
+++ app/src/main/java/org/d3if3136/assessment02/model/HasilInput.kt
@@ -0,0 +1,5 @@
+package org.d3if3136.assessment02.model
+
+data class HasilInput(
+    val lanjut: String
+)
